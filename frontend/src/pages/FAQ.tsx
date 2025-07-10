ce71ac5ff7bcd7244d6807dd5892a09ec5abdb4c
aira-syfting <aira@syftingit.com>
2025-07-09 22:20:28 -0400

    HashRouter and image routing (#42)
    * use HashRouter
    
    * image routing
    
    ---------
    
    Co-authored-by: aira-syfting <aira@syfting.com>

--- frontend/src/pages/FAQ.tsx
+++ frontend/src/pages/FAQ.tsx
@@ -6,7 +6,7 @@ const FAQ: React.FC = () => {
       <div className="pt-12 flex flex-col items-center space-y-6">
         {/* Bigger & higher image */}
         <img
-          src="/assets/29-badge2.png"
+          src="./assets/29-badge2.png"
           alt="Spinning Logo"
           className="h-40 w-40 animate-spin [animation-duration:10s]"
         />
