50c2c31d412584b04dc8c34d46032123abb4d779
aira-syfting <aira@syftingit.com>
2025-10-28 12:54:30 -0400

    launch page mobile view layout (#71)
    * use HashRouter
    
    * image routing
    
    * update package.json
    
    * update for Vercel deployment
    
    * update for Vercel deployment (#44)
    
    * use HashRouter
    
    * use HashRouter (#41)
    
    Co-authored-by: aira-syfting <aira@syfting.com>
    
    * HashRouter and image routing (#42)
    
    * use HashRouter
    
    * image routing
    
    ---------
    
    Co-authored-by: aira-syfting <aira@syfting.com>
    
    ---------
    
    Co-authored-by: aira-syfting <aira@syfting.com>
    
    * fix routing
    
    * routing
    
    * add dependecies
    
    * website tab title
    
    * favicon
    
    * login and register pages
    
    * homepage initial commit
    
    * remove unused import
    
    * homepage
    
    * location grid yet again
    
    * backend changes
    
    * top nav hover yet again
    
    * popup and other changes
    
    * homepage and popup
    
    * about page
    
    * about page and favicon
    
    * login popup
    
    * exit login button
    
    * login and register setup
    
    * delete unused import
    
    * login and profile page
    
    * login; trying to add cookies
    
    * ok I actually figured it out
    
    * settings page
    
    * change prod variable
    
    * host db
    
    * login changes
    
    * fix cookie settings for prod
    
    * cookies
    
    * cookies
    
    * city tabs update
    
    * add db changes
    
    * library import for db change
    
    * newsletter signup
    
    * email signup fix
    
    * put back password page
    
    * scroll to top
    
    * launch page changes
    
    * launch page styling changes
    
    ---------
    
    Co-authored-by: aira-syfting <aira@syfting.com>

--- frontend/src/pages/LaunchHomepage.tsx
+++ frontend/src/pages/LaunchHomepage.tsx
@@ -53,25 +53,67 @@ const LaunchHomepage: React.FC = () => {
     
   return (
     <div className="flex items-center justify-center min-h-screen w-screen bg-deepRed text-light font-monaco font-semibold">
-        <div className="flex-1 ml-[8rem]">
+        {/* show only on large screens */}
+        <div className="
+                hidden md:block
+                flex-1 ml-[8rem]
+            ">
             <img
             src="/assets/3-badge1.png"
             alt="Syfting Badge Logo"
             className="h-[30rem] w-auto"
             />
         </div>
-        <div className="flex-1 mr-[8rem]">
+
+        <div className="
+                flex-1 mr-[2rem] ml-[2rem]
+                md:mr-[8rem]
+            ">
             <div className="flex justify-center">
                 <img
                 src="/assets/mainlogo-brightRed.png"
                 alt="Syfting Main Logo Bright Red"
+<<<<<<< HEAD
+                className="
+                    h-auto w-[15rem] object-center
+                    md:w-[27rem]
+                "
+=======
                 className="h-auto w-[27rem] object-center"
+>>>>>>> origin/master
                 />
             </div>
+
+            {/* show only on mobile */}
+            <div className="
+                    md:hidden flex justify-center mb-4
+                ">
+                <img
+                src="/assets/3-badge1.png"
+                alt="Syfting Badge Logo"
+                className="h-[15rem] w-auto"
+                />
+            </div>
+
             <div>
+<<<<<<< HEAD
+                <h1 className="
+                    text-[1.2rem] text-center
+                    md:text-[1.8rem]
+                ">We're launching soon...</h1>
+                <h1 className="
+                    text-[1.2rem] text-center
+                    md:text-[1.8rem]
+                ">Join the waitlist that's baking</h1>
+                <h1 className="
+                    text-[1.2rem] text-center
+                    md:text-[1.8rem]
+                ">the future.</h1>
+=======
                 <h1 className="text-[1.8rem] text-center">We're launching soon...</h1>
                 <h1 className="text-[1.8rem] text-center">Join the waitlist that's baking</h1>
                 <h1 className="text-[1.8rem] text-center">the future.</h1>
+>>>>>>> origin/master
             </div>
             <div>
                 <form onSubmit={handleSubmit} className="flex flex-col justify-center mt-6">
@@ -84,12 +126,22 @@ const LaunchHomepage: React.FC = () => {
                                 value={formData.email}
                                 onChange={handleChange}
                                 required
+<<<<<<< HEAD
+                                className="
+                                    w-full p-2 text-light text-[0.8rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none
+                                    md:text-[1.3rem]
+                                "
+=======
                                 className="w-full p-2 text-light text-[1.3rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none"
+>>>>>>> origin/master
                             />
                         </div>
                     </div>
 
-                    <div className="flex gap-[2rem] mb-[1.5rem]">
+                    <div className="
+                            flex gap-[0.7rem] mb-[1.5rem]
+                            md:gap-[2rem]
+                        ">
                         <div className="flex-1 flex flex-col">
                              <input
                                 type="text"
@@ -98,7 +150,14 @@ const LaunchHomepage: React.FC = () => {
                                 value={formData.first_name}
                                 onChange={handleChange}
                                 required
+<<<<<<< HEAD
+                                className="
+                                    w-full p-2 text-light text-[0.8rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none
+                                    md:text-[1.3rem]
+                                "
+=======
                                 className="w-full p-2 text-light text-[1.3rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none"
+>>>>>>> origin/master
                             />
                         </div>
                         <div className="flex-1 flex flex-col">
@@ -109,18 +168,35 @@ const LaunchHomepage: React.FC = () => {
                                 value={formData.last_name}
                                 onChange={handleChange}
                                 required
+<<<<<<< HEAD
+                                className="
+                                    w-full p-2 text-light text-[0.8rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none
+                                    md:text-[1.3rem]
+                                "
+=======
                                 className="w-full p-2 text-light text-[1.3rem] uppercase border-b border-light bg-transparent placeholder:text-light focus:outline-none"
+>>>>>>> origin/master
                             />
                         </div>
                     </div>
 
-                    <div className="flex gap-[1rem] mt-8">
+                    <div className="
+                            flex gap-[0.4rem] 
+                            md:mt-8 md:gap-[1rem]
+                        ">
                         {roles.map((role) => (
                             <button
                             key={role}
                             type="button"
                             onClick={() => setFormData({ ...formData, role })}
+<<<<<<< HEAD
+                            // padding makes badge logo appear/disappear
+                            className={`
+                                flex-1 text-[0.7rem] py-2 rounded-full border border-light transition-colors duration-200
+                                md:text-[1.3rem] md:px-4
+=======
                             className={`flex-1 text-[1.3rem] px-4 py-2 rounded-full border border-light transition-colors duration-200
+>>>>>>> origin/master
                                 ${
                                     formData.role === role
                                     ? "bg-light text-deepRed border-deepRed"
@@ -137,7 +213,9 @@ const LaunchHomepage: React.FC = () => {
                         <button
                             type="submit"
                             disabled={!allFilled || isSubmitting}
-                            className={`w-auto text-beige text-[1.5rem] px-8 py-4 mt-[2rem] rounded-full transition-colors duration-200
+                            className={`
+                                w-auto text-beige text-[1rem] px-6 py-2 mt-[2rem] rounded-full transition-colors duration-200
+                                md:text-[1.5rem] md:px-8 md:py-4
                                 ${
                                 allFilled && !isSubmitting
                                     ? "bg-brightRed hover:opacity-90 cursor-pointer"
